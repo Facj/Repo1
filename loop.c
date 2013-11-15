de5d18e0c5440080a19ae26258f8dd151dd85b4e
Facj <fa.castro.jul@gmail.com>
2013-11-15 15:43:11 +0200

    v3.3

--- loop.c
+++ loop.c
@@ -3,7 +3,7 @@
 
 int main()
 {
-  55dddd
+  
   time_t rawtime;
    struct tm *timeinfo;
    FILE *fp;
@@ -13,7 +13,7 @@ int main()
    time (&rawtime);
    timeinfo = localtime (&rawtime);
    fp=fopen("version_record.txt","a");
-    fprintf(fp,"Version 3.2     %s",asctime(timeinfo));
+    fprintf(fp,"Version 3.3     %s",asctime(timeinfo));
    fclose(fp);
    }
  

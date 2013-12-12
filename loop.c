f5538816d316493e128624b2ec7d79201a025427
Facj <fa.castro.jul@gmail.com>
2013-12-12 15:53:00 +0200

    v3.3.7

--- loop.c
+++ loop.c
@@ -12,7 +12,7 @@ int main()
    time (&rawtime);
    timeinfo = localtime (&rawtime);
    fp=fopen("version_record.txt","a");
-    fprintf(fp,"Version 3.3.6  signed   %s",asctime(timeinfo));
+    fprintf(fp,"Version 3.3.7  signed   %s",asctime(timeinfo));
    fclose(fp);
    }
  

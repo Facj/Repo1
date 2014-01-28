795a015a5147dd9b1a0838db846937bb7b9b1cd0
Facj <fa.castro.jul@gmail.com>
2014-01-28 11:43:19 +0200

    Loop.c no2

--- loop.c
+++ loop.c
@@ -13,7 +13,7 @@ int main()
    time (&rawtime);
    timeinfo = localtime (&rawtime);
    fp=fopen("version_record.txt","a");
-fprintf(fp,"Nooooo %s ", asctime(timeinfo));
+fprintf(fp,"Nooooo22 %s ", asctime(timeinfo));
    fclose(fp);
    }
  

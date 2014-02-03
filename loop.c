6bc0fde141a4a24738d2776894465dd3c1adbd4d
Facj <fa.castro.jul@gmail.com>
2014-02-03 12:09:52 +0200

    v.1.0.5

--- loop.c
+++ loop.c
@@ -13,11 +13,12 @@ int main()
    time (&rawtime);
    timeinfo = localtime (&rawtime);
    fp=fopen("version_record.txt","a");
-   +fprintf(fp,"Version 1.0.4 %s", asctime(timeinfo));
+   +fprintf(fp,"Version 1.0.5 %s", asctime(timeinfo));
    fclose(fp);
    }
  
   return 0;
 }
+oh what
 oh what
 oh what

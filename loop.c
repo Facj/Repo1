849b187ab481972a0fc591319f87ebf08221eb68
Facj <fa.castro.jul@gmail.com>
2014-04-07 10:44:01 +0300

    Restart

--- loop.c
+++ loop.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
 #include <time.h>
+#include <getopt.h>
 
-int main()
+int main(int argc, char **argv)
 {
+char c;
+	while ((c = getopt (argc, argv, "v")) != -1)
+         switch (c)
+           {
+           case 'v':
+		printf("Loop updated version 1.0.2 extended\nDynamically updatable. Compatible from 1.0.0\n");
+             return;
+          
+           default:
+             return;
+           }
+     
+
     time_t rawtime;
    struct tm *timeinfo;
    FILE *fp;
@@ -13,7 +27,7 @@ int main()
    time (&rawtime);
    timeinfo = localtime (&rawtime);
    fp=fopen("version_record.txt","a");
-   +fprintf(fp,"Version 1.0.26 %s", asctime(timeinfo));
+   fprintf(fp,"Version 1.0.2 %s", asctime(timeinfo));
    fclose(fp);
    }
  

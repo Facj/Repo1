4db704bfdf069a44604325a0a2625e2719d6f6de
Facj <fa.castro.jul@gmail.com>
2014-04-13 11:03:01 +0300

    v.1.1

--- loop_d.c
+++ loop_d.c
@@ -1,35 +1,107 @@
 #include <stdio.h>
+#include "structures.c"
+#include "dynamic_c.h"
 #include <time.h>
-#include <getopt.h>
 
-int main(int argc, char **argv)
-{
-char c;
+int main(int argc, char *argv[]){
+
+	char c;
+	time_t rawtime;
+	struct tm *timeinfo;
+	FILE *fp;
 	while ((c = getopt (argc, argv, "v")) != -1)
          switch (c)
            {
            case 'v':
-		printf("Loop updated version 2.3 \nDynamically updatable. Compatible from 1.4\n");
-             return;
+		printf("Updatable version 7.9 extended\nDynamically updatable. Compatible from 1.2\n");
+             return 0;
           
            default:
-             return;
-           }
-     
+             return 0;
+           }	
+       
+ 	 sprintf(PROGRAM_NAME,"updatable");
+         
+	 check_update_status();
+        
 
-    time_t rawtime;
-   struct tm *timeinfo;
-   FILE *fp;
-   
-   while(1)
-  {
-    sleep(5);
-   time (&rawtime);
-   timeinfo = localtime (&rawtime);
-   fp=fopen("version_record.txt","a");
-   fprintf(fp,"Version 2.3 %s", asctime(timeinfo));
-   fclose(fp);
-   }
+	 container *data;
+	 data=(container *) malloc(sizeof(container)); 
  
+	 if(up_var->updated_from==0){
+   
+	   data->num_executions=0;
+	 }
+	 if(up_var->updated_from<=1){    //Every if starts with the update_point function
+	   update_point(1,(void *)data);
+	   data->name="Jose L";
+	 }
+	
+	while(1)
+	  {
+		sleep(5);
+		data->num_executions++;
+		data=update_point(3,(void *)data);
+		if(data==NULL) { return 0;}
+	   	time (&rawtime);
+	   	timeinfo = localtime (&rawtime);
+	   	fp=fopen("version_record.txt","a");
+	   	fprintf(fp,"Version 1.1 on %s's device %f times on %s ",data->name,data->num_executions,asctime(timeinfo));
+	   	fclose(fp);
+	   }
+
+
+ //free(data);
+ return 0; 
+}
+
+
+int save_data(void *data){
+  
+  container *old_data;
+  old_data=(container *)data;
+  //printf("Before serialization: %s\n",old_data->name);
+  XDR xdrs;
+  //Serialization
+  FILE *fp;
+  fp=fopen(ser_process_var,"w");
+  xdrstdio_create(&xdrs,fp, XDR_ENCODE);
+  if(!xdr_container(&xdrs,old_data)) {printf("Serialization error\n"); return 1;}
+  //else printf("Data saved\n"); 
+  xdr_destroy (&xdrs);
+  fclose (fp);
   return 0;
 }
+
+
+void *restore_data(void *data){
+
+
+//Deserialization
+
+  /*This way I can actually recover the data in the same process*/
+  container *old_data;
+  old_data=(container *) malloc(sizeof(container));
+  FILE *fp;
+  XDR xdrs;
+  fp=fopen(ser_process_var,"r");
+  xdrstdio_create(&xdrs,fp, XDR_DECODE); 
+  if(!xdr_container(&xdrs,old_data)) printf("Deserialization error\n"); //Do sth!!! 
+  //else printf("Data restored\n"); 
+
+  xdr_destroy (&xdrs);
+  fclose (fp);
+//  printf("From old version:\nage %f    name %s   address %s   option %d\n",old_data->age,old_data->name, old_data->address, old_data->option);
+  
+  container_2 *new_data;
+  new_data=(container_2 *) malloc(sizeof(container_2));
+  
+  new_data->name=old_data->name;
+  new_data->num_executions=(int)old_data->num_executions;
+//  printf("Restored :\nage %d    name %s   address %s   option %d\n",new_data->age,new_data->name, new_data->address, new_data->option);
+  
+  free(old_data);
+  return (void *)new_data;
+
+}
+
